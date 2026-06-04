82474c2e244018b3e1b85274abfe98e40dbe3004
maxbear1000-debug <maxbear1000@gmail.com>
2026-06-04 21:14:04 +0000

    fix

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ function SelectFighter(name) {
   text.innerHTML = `You selected <span style="color: yellow">${name}</span>!`;
 
   const fighter = mathupInfo[name];
-  const matchupInfo = document.getElementById("matchupInfo");
+  const infoBox = document.getElementById(`${name}-info`);
 
-  if (fighter && matchupInfo) {
-    matchupInfo.innerHTML = `
-      <h3>Good Matchup</h3>
-      <p>${fighter.good.join(", ")}</p>
-      <h3>Bad Matchups</h3>
-      <p>${fighter.bad.join(", ")}</p>
-    `;
-  }
+  
+  infoBox.innerHTML = `
+    <h3>Good Matchups</h3>
+    <p>${fighter.good.join(", ")}</p>
 
-  playSound();
+    <h3>Bad Matchups</h3>
+    <p>${fighter.bad.join(", ")}</p>
+  `;
 }
 
 const mathupInfo = {
